0cb0812372cd730327dd3d91971c7ccd610db0e4
devd254 <devond156@gmail.com>
2024-02-01 16:15:39 -0500

    animated whale

--- src/models/Whale.jsx
+++ src/models/Whale.jsx
@@ -1,14 +1,21 @@
-import React, { useRef } from "react";
-import { useGLTF } from "@react-three/drei";
-
+import React, { useEffect } from "react";
+import { useAnimations, useGLTF } from "@react-three/drei";
 import whale from "../assets/animated_whale.glb";
 
 const Whale = () => {
     const { scene, animations } = useGLTF(whale);
+    const { ref, actions, names } = useAnimations(animations);
+    
+    useEffect(() => {
+        actions[names[0]].play()
+        return () => actions[names[0]]
+    }, [actions, names])
     return(
-        <mesh position={[-10, 20, 1]} scale={[2.3, 2.3, 2.3]}>
-            <primitive object={scene} />
-        </mesh>
+        <group ref={ref} dispose={null}>
+            <mesh position={[-10, 20, 1]} scale={[2.3, 2.3, 2.3]}>
+                <primitive object={scene} />
+            </mesh>
+        </group>        
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
     <section className='w-full h-screen relative'>
         <Canvas 
           className='w-full h-screen bg-transparent'
-          camera={{ near: 0.1, far: 1000 }}
+          camera={{ position: [-120.0, 100, 100.0], near: 0.1, far: 1000 }}
         >
             <OrbitControls />
             <Suspense fallback={<Loader />}>
