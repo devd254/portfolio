2ce2216fced872af17357abc801dca602bff7d00
devd254 <devond156@gmail.com>
2024-02-16 22:22:16 -0500

    bubble adds

--- src/models/Bubble.jsx
+++ src/models/Bubble.jsx
@@ -4,7 +4,7 @@ const Bubble = ({ position, size }) => {
     const [width, height] = size;
     const {x, y} = position;
     return(
-        <div className={`w-${width} h-${height}`}
+        <div className={`relative w-${width} h-${height} bottom-0 left-0`}
         >
             <div className="bubble"/>
 
