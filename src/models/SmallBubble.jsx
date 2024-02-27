48544b672e480206c6e82bab3ead5c612d066677
devd254 <devond156@gmail.com>
2024-02-27 17:45:13 -0500

    smallbubble loop

--- src/models/SmallBubble.jsx
+++ src/models/SmallBubble.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 
-const SmallBubble = () => {
+const SmallBubble = ({ x_position }) => {
+    const x = x_position;
     return(
         <div>
             <div className={`relative flex w-60 h-60 off-up right-10`}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import SmallBubble from '../models/SmallBubble';
 import Bubble from '../models/Bubble';
 
 const About = ({ isOpen }) => {
+  const small_bubbles = [];
+  for (let i = 0; i < 100; i += 40) {
+      small_bubbles.push(<SmallBubble x_position={i} />);
+  }
   return (
     <section className="prevent-scroll h-screen flex bg-gradient-to-b to-yellow-100 from-cyan-300">
       <Bubble />
+      <tbody>{small_bubbles}</tbody>
+
     </section>
   )
 }
