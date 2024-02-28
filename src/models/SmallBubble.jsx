0f15690cf406935b09d49db64737db0f11caf5c3
devd254 <devond156@gmail.com>
2024-02-28 12:42:04 -0500

    Bubble animation complete

--- src/models/SmallBubble.jsx
+++ src/models/SmallBubble.jsx
@@ -1,17 +1,17 @@
 import React from "react"
 
-const SmallBubble = ({ x_position, delay }) => {
+const SmallBubble = ({ x_position, delay, direction, duration }) => {
     const x = x_position;
     const d = delay;
+    const direct = direction;
+    const time = duration;
+    console.log(time);
     return(
         <div>
-            <div className={`relative flex w-40 h-40 above-screen bubble-small-rise`} style={{ left:`${x}px`}}
+            <div className={`fixed flex w-40 h-40 above-screen ${direct}`} style={{ left:`${x}px` }}
             >
-                {/* Works when side-float div is removed */}
-                {/* <div className="side-float"> */}
-                    <div className="bubble-small" style={{ animationDelay: `${d}s` }}>
+                    <div className="bubble-small" style={{ animationDelay: `${d}s`, animationDuration: `${time}s` }}>
                     </div>
-                {/* </div> */}
             </div>
         </div>
     )
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,31 +3,111 @@ import SmallBubble from '../models/SmallBubble';
 import Bubble from '../models/Bubble';
 
 const About = ({ isOpen }) => {
+  //Bubble rows
   const small_bubbles1 = [];
   const small_bubbles2 = [];
   const small_bubbles3 = [];
   const small_bubbles4 = [];
   const small_bubbles5 = [];
+  const small_bubbles6 = [];
+  const small_bubbles7 = [];
+  const small_bubbles8 = [];
   let delay = 0.2;
-  for (let i = 0; i <= 1000; i += 200) {
-      small_bubbles1.push(<SmallBubble x_position={i} delay={delay} />);
+  // Choosing Bubble
+  let left = 'bubble-swirl-left';
+  let right = 'bubble-swirl-right';
+  let numIteration = 0;
+  let choose;
+  const minCeiled = Math.ceil(2.0);
+  const maxFloored = Math.floor(6.0);
+  //Randomize duration(speed)
+  for (let i = -100; i <= 1800; i += 150) {
+      choose = numIteration % 2 == 0 ? left : right; 
+      numIteration += 1
+      small_bubbles1.push(<SmallBubble x_position={i}
+                                       delay={delay} 
+                                       direction={choose} 
+                                       duration={Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)}
+                          />);
   }
+  numIteration = 0;
   delay += 0.1;
-  for (let i = 100; i <= 1100; i += 200) {
-      small_bubbles2.push(<SmallBubble x_position={i} delay={delay} />);
+  for (let i = 0; i <= 1900; i += 150) {
+      choose = numIteration % 2 == 0 ? left : right; 
+      numIteration += 1
+      small_bubbles2.push(<SmallBubble x_position={i}
+                                       delay={delay} 
+                                       direction={choose}
+                                       duration={Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)}
+                          />);
   }
+  numIteration = 0;
   delay += 0.1;
-  for (let i = 0; i <= 1000; i += 200) {
-      small_bubbles3.push(<SmallBubble x_position={i} delay={delay} />);
+  for (let i = -100; i <= 1800; i += 150) {
+      choose = numIteration % 2 == 0 ? left : right; 
+      numIteration += 1
+      small_bubbles3.push(<SmallBubble x_position={i}
+                                       delay={delay}
+                                       direction={choose}
+                                       duration = {Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)}
+                          />);
   }
+  numIteration = 0;
   delay += 0.1;
-  for (let i = 100; i <= 1100; i += 200) {
-      small_bubbles4.push(<SmallBubble x_position={i} delay={delay} />);
+  for (let i = 0; i <= 1900; i += 150) {
+      choose = numIteration % 2 == 0 ? left : right; 
+      numIteration += 1
+      small_bubbles4.push(<SmallBubble x_position={i}
+                                       delay={delay}
+                                       direction={choose}
+                                       duration = {Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)}
+                          />);
   }
+  numIteration = 0;
   delay += 0.1;
-  for (let i = 0; i <= 1000; i += 200) {
-      small_bubbles5.push(<SmallBubble x_position={i} delay={delay} />);
+  for (let i = -100; i <= 1800; i += 150) {
+      choose = numIteration % 2 == 0 ? left : right; 
+      numIteration += 1
+      small_bubbles5.push(<SmallBubble x_position={i}
+                                       delay={delay}
+                                       direction={choose}
+                                       duration = {Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)}
+                          />);
   }
+  numIteration = 0;
+  delay += 0.1;
+  for (let i = 0; i <= 1800; i += 150) {
+      choose = numIteration % 2 == 0 ? left : right; 
+      numIteration += 1
+      small_bubbles6.push(<SmallBubble x_position={i}
+                                       delay={delay}
+                                       direction={choose}
+                                       duration = {Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)}
+                          />);
+  }
+  numIteration = 0;
+  delay += 0.1;
+  for (let i = -100; i <= 1800; i += 150) {
+      choose = numIteration % 2 == 0 ? left : right; 
+      numIteration += 1
+      small_bubbles7.push(<SmallBubble x_position={i}
+                                       delay={delay}
+                                       direction={choose}
+                                       duration = {Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)}
+                          />);
+  }
+  numIteration = 0;
+  delay += 0.1;
+  for (let i = 0; i <= 1800; i += 150) {
+      choose = numIteration % 2 == 0 ? left : right; 
+      numIteration += 1
+      small_bubbles8.push(<SmallBubble x_position={i}
+                                       delay={delay}
+                                       direction={choose}
+                                       duration = {Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)}
+                          />);
+  }
+  
   // Random variables: amplitude(side to side), speed rise, size bubble
   return (
     <section className="prevent-scroll h-screen flex bg-gradient-to-b to-yellow-100 from-cyan-300">
@@ -37,6 +117,9 @@ const About = ({ isOpen }) => {
       {small_bubbles3}
       {small_bubbles4}
       {small_bubbles5}
+      {small_bubbles6}
+      {small_bubbles7}
+      {small_bubbles8}
     </section>
   )
 }
