799329a9f8964b3db124faa212af3888f2b31b6e
unknown <devond156@gmail.com>
2024-03-05 20:49:44 -0500

    new model wrong colors

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,7 +9,7 @@ const About = ({ isOpen }) => {
     <div className='relative w-full h-screen prevent-scroll flex bg-gradient-to-b to-blue-200 from-cyan-300'>
       <Canvas
         className='w-full h-full absolute top-0 left-0'
-        camera={{ position: [-20.0, 5, 20.0], near: 0.1, far: 1000 }}
+        camera={{ position: [-5.0, 1.25, 5.0], near: 0.1, far: 1000 }}
       >
         <ambientLight
           position={[0.0, 10.0, 0.0]}
