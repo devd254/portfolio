588a4eb3829bfb0ce3dfddc1163c631e18cb3e0d
unknown <devond156@gmail.com>
2024-03-24 19:51:34 -0400

    vulnerability fix

--- src/models/ContactForm.jsx
+++ src/models/ContactForm.jsx
@@ -25,9 +25,9 @@ const ContactForm = () => {
     return (
         <form ref={form} onSubmit={sendEmail}>
         <label>Name</label>
-        <input type="text" name="user_name" />
+        <input type="text" name="userName" />
         <label>Email</label>
-        <input type="email" name="user_email" />
+        <input type="email" name="userEmail" />
         <label>Message</label>
         <textarea name="message" />
         <input type="submit" value="Send" />
