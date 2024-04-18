9d38d241f4fd2ad8bb94c42198152fa46a3752f1
devd254 <devond156@gmail.com>
2024-04-18 17:45:05 -0400

    Start on Loader change

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,7 +5,10 @@ import { Loader2 } from 'lucide-react';
 const Loader = () => {
   return (
     <Html>
-        <div className='flex justify-center items-center'>
+        <div className='flex-col justify-center items-center relative top-[-5rem]'>
+            <p className='relative left-3'>
+                  Diving
+            </p>
             <Loader2 
               color='#00FFFF'
               className='bg-red-200 w-20 h-20 border-2 border-opacity-50 border-blue-600 border-t-gray-600 rounded-full animate-spin'
